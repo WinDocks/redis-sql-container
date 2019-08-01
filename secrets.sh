9df2fc2d42eb1746a8f606e9925bf417b75fd13e
windocks <dev@windocks.com>
2019-08-01 11:24:19 -0700

    sa

--- secrets.sh
+++ secrets.sh
@@ -1,7 +1,7 @@
 
 # You must run these commands for the Windocks Sql proxy to connect to the Windocks server to creatre SQL Server containers
 
-kubectl create secret generic proxy-secrets --from-literal=WINDOCKS_REQUIRED_USERNAME='user2' --from-literal=WINDOCKS_REQUIRED_PASSWORD='WCC!!tbBEfN1' --from-literal=WINDOCKS_REQUIRED_CONTAINER_SAPASSWORD='sa-password-to-set-on-sql-container'
+kubectl create secret generic proxy-secrets --from-literal=WINDOCKS_REQUIRED_USERNAME='user2' --from-literal=WINDOCKS_REQUIRED_PASSWORD='WCC!!tbBEfN1' --from-literal=WINDOCKS_REQUIRED_CONTAINER_SAPASSWORD='yat!834sd'
 
 
 # Put your key and crt in the file or just run it with the dummy crt and key if you are not intertested in tls
